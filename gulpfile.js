7113ccc15df2f2b0c25553c67cc35a0d27a98eef
umang kharbanda <ukharbanda@yahoo.co.in>
2014-12-19 18:10:58 +0530

    Combined all js files into script.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,27 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
+
+var jsSources;
+
+jsSources = [
+	'components/scripts/affix.js',
+	'components/scripts/alert.js',
+	'components/scripts/button.js',
+	'components/scripts/carousel.js',
+	'components/scripts/collapse.js',
+	'components/scripts/dropdown.js',
+	'components/scripts/modal.js',
+	'components/scripts/popover.js',
+	'components/scripts/scrollspy.js',
+	'components/scripts/tab.js',
+	'components/scripts/tooltip.js',
+	'components/scripts/transition.js',
+	'components/scripts/typed.js'
+];
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
+});
